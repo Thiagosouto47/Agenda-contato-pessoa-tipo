64fb6a355aafeb6aa6462b26abf122412e831e06
Thiago Souto <thiagocsouto@outlook.com>
2022-04-28 10:14:22 -0500

    Update schema-h2.sql

--- agenda/agenda/src/main/resources/schema-h2.sql
+++ agenda/agenda/src/main/resources/schema-h2.sql
@@ -1,6 +1,6 @@
-create table pessoa(id bigint auto_increment, nome varchar(100), cpf varchar(20), rg varchar(15), sexo varchar(1));
+create table pessoa(id bigint auto_increment, nome varchar(60), cpf varchar(12), rg varchar(14), sexo varchar(1));
 create table contato(id bigint auto_increment, descr_contato varchar(50), pessoa_id integer, tipo_id integer);
-create table tipo(id bigint auto_increment, tipo_contato varchar(30));
+create table tipo(id bigint auto_increment, tipo_contato varchar(35));
 
 Alter table contato add constraint fk_pessoa foreign key(pessoa_id)
 references pessoa(pessoa_id);
