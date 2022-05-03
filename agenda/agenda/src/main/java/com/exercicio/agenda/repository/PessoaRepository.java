b94323fcd545fd4ae0b9df4f98d0ef349b9726a8
Thiago Souto <thiagocsouto@outlook.com>
2022-05-03 12:54:24 -0500

    Delete agenda/agenda directory

--- agenda/agenda/src/main/java/com/exercicio/agenda/repository/PessoaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exercicio.agenda.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.exercicio.agenda.entity.Pessoa;
-
-public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
-
-}
--- agenda/agenda/src/main/java/com/exercicio/agenda/repository/TipoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exercicio.agenda.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.exercicio.agenda.entity.Tipo;
-
-public interface TipoRepository extends JpaRepository<Tipo, Integer>{
-
-}
