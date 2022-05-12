75ed47fe606cf2eed047009a328017c498863e5c
Thiago Souto <thiagocsouto@outlook.com>
2022-05-12 10:16:47 -0400

    Delete agenda/agenda directory

--- agenda/agenda/src/main/resources/schema-h2.sql
+++
@@ -1,9 +0,0 @@
-create table pessoa(id bigint auto_increment, nome varchar(60), cpf varchar(14), rg varchar(14), sexo varchar(1));
-create table contato(id bigint auto_increment, descr_contato varchar(50), pessoa_id integer, tipo_id integer);
-create table tipo(id bigint auto_increment, tipo_contato varchar(35));
-
-Alter table contato add constraint fk_pessoa foreign key(pessoa_id)
-references pessoa(pessoa_id);
-
-Alter table contato add constraint fk_contato foreign key(tipo_id)
-references tipo(tipo_id);
